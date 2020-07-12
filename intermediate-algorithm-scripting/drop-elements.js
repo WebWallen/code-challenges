9f0ed28f20431e2c7004d534c78a0a0adc3ae4f2
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-07-11 20:56:27 -0400

    Clarify response to given input.

--- intermediate-algorithm-scripting/drop-elements.js
+++ intermediate-algorithm-scripting/drop-elements.js
@@ -2,8 +2,11 @@
 function dropElements(arr, func) {
 
     while (arr.length > 0 && !func(arr[0])) {
-        arr.shift(); // removes zeroth element, scoots others
+        arr.shift(); // removes zeroth element, scoots others to left 
     }
 
     return arr;
 }
+
+dropElements([1, 2, 3, 4, 5, 6], i > 3);
+// Returns [4, 5, 6]
