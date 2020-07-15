f2a825497a1ac163b575b8724e3d944a08298f26
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-07-15 18:51:23 -0400

    Finders Keepers Code Challenge.

---
+++ basic-algorithm-scripting/finders-keepers.js
@@ -0,0 +1,9 @@
+function findElement(arr, func) {
+    for (let num of arr) {
+      if (func(num) === true) return num;
+    }
+    return undefined;
+  }
+  
+findElement([1, 2, 3, 4], num => num % 2 === 0);
+// Returns 2
