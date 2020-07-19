da2959aeb6b57a87180e7682e3b86afb026effbc
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-07-18 20:03:26 -0400

    Where I (index) belong code challenge.

---
+++ basic-algorithm-scripting/where-i-belong.js
@@ -0,0 +1,14 @@
+function getIndexToIns(arr, num) {
+    let sorted = arr.sort((a, b) => a - b);
+
+    for (let i = 0; i < sorted.length; i++) {
+        // Belongs at index before the first bigger number
+        if (sorted[i] >= num) return i;
+    }
+
+    // In case it goes at the end
+    return sorted.length;
+  }
+  
+  getIndexToIns([40, 60], 50);
+  // Returns 1
