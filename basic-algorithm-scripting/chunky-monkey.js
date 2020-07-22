96cf1879fc1a27470f6645acf04d939e84d6ec9f
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-07-21 20:08:50 -0400

    Chunk array into subarrays code challenge.

---
+++ basic-algorithm-scripting/chunky-monkey.js
@@ -0,0 +1,13 @@
+function chunkArrayInGroups(arr, size) {
+    let newArr = [];
+  
+    for (let i = 0; i < arr.length; i += size) {\
+    //   Loop is not here to go through every index, just to specify the size of subarray
+      newArr.push(arr.slice(i, i + size));
+    }
+  
+    return newArr;
+  }
+  
+  chunkArrayInGroups(["a", "b", "c", "d"], 2);
+//   Returns [[a, b], [c, d]]
