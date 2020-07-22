7a2ab01dff490a91ca42761bcc26045b5a363cc2
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-07-21 20:09:33 -0400

    Whoops, typo.

--- basic-algorithm-scripting/chunky-monkey.js
+++ basic-algorithm-scripting/chunky-monkey.js
@@ -1,7 +1,7 @@
 function chunkArrayInGroups(arr, size) {
     let newArr = [];
   
-    for (let i = 0; i < arr.length; i += size) {\
+    for (let i = 0; i < arr.length; i += size) {
     //   Loop is not here to go through every index, just to specify the size of subarray
       newArr.push(arr.slice(i, i + size));
     }
