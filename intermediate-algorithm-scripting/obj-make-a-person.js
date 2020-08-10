d8bc0c8c8135c4da5c0e832e0b3bd35722ea5b60
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-08-10 15:43:54 -0400

    Back into daily (hard) coding -- I (soft) code all the time -- with a code challenge that involves creating a Person object and associated methods to get/set their first, last, and full names.

---
+++ intermediate-algorithm-scripting/obj-make-a-person.js
@@ -0,0 +1,21 @@
+var Person = function(firstAndLast) {
+  let fullName = firstAndLast;
+  this.getFirstName = () => fullName.split(' ')[0];
+  this.getLastName = () => fullName.split(' ')[1];
+  this.getFullName = () => fullName;
+  
+  this.setFirstName = function(first) {
+    fullName = first + " " + fullName.split(' ')[1]
+  }
+
+  this.setLastName = function(last) {
+    fullName = fullName.split(' ')[0] + " " + last;
+  }
+
+  this.setFullName = function(name) {
+    fullName = name;
+  }
+};
+
+var bob = new Person('Bob Ross');
+bob.getFullName();
